7e3cdca6c424e309f42f17ec0808a8436723a09e
angellam278 <88823308+angellam278@users.noreply.github.com>
2022-03-29 21:05:34 -0400

    fixed brdf

--- PathTracing/src/main.cpp
+++ PathTracing/src/main.cpp
@@ -164,7 +164,7 @@ void InitializePathTracer()
 	pathTracer.SetCamera(glm::vec3(0.0f, 0.0f, -9.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	
 	pathTracer.LoadMesh("sphere.obj.object", glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 0.0f), 1.0f, 1.0f);
-    pathTracer.LoadMesh("cube.obj.object", glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(4.0f)), glm::vec3(0.0f, -3.0f, 0.0f)), glm::vec3(1.0f, 1.0f, 0.0f), 1.0f, 0.5f);
+    pathTracer.LoadMesh("cube.obj.object", glm::translate(glm::rotate(glm::scale(glm::mat4(1.0f), glm::vec3(1.0f)), 45.0f, glm::vec3(1.0, 0.0, 0.0)), glm::vec3(0.0f, -2.0f, 0.0f)), glm::vec3(1.0f, 1.0f, 0.0f), 1.0f, 0.5f);
     pathTracer.AddLight(glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3(1.0f));
 }
 
